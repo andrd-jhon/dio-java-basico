9eed6a1ade4244a1bd8b4d322ee4a628aaab50ea
andrd-john <jv-andrade02@outlook.com>
2024-04-27 09:30:21 -0300

    tipos de variaveis foi adicionado

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,14 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+        // System.out.println(numero);
+    }
+}
