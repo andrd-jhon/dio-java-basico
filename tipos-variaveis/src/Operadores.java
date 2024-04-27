97df0ac8190c34c3a798e1603b97cd5c70f6e103
andrd-john <jv-andrade02@outlook.com>
2024-04-27 11:21:13 -0300

    tipos de operadores adcionados

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,48 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // int numero = 5;
+
+        // System.out.println(-numero);
+        // System.out.println(numero);
+
+        // numero = -numero;
+        // System.out.println(numero);
+
+        // numero = +numero;//- com + é igual a -
+        // System.out.println(numero);
+
+        // numero = -numero;
+        // System.out.println(numero);
+
+        // System.out.println(-- numero);
+
+        // boolean variavel = true;
+        // System.out.println(!variavel);
+
+        // int numero1 = 10;
+        // int numero2 = 20;
+
+        // boolean simNao = numero1 == numero2;
+
+        // System.out.println("O primeiro número é igual ao segundo? " + simNao);
+
+        // simNao = numero1 != numero2;
+
+        // System.out.println("O primeiro número é diferente do segundo? " + simNao);
+
+        // if (numero1 < numero2){
+        //     System.out.println("Nossa condição é verdadeira!");
+        // }
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras!");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira!");
+        }
+        System.out.println("fim!");
+    }
+}
